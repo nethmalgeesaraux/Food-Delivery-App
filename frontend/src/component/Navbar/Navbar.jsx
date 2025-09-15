6b6fadb419fdcea42dbe7b31d6a7df16e489b3be
nethmalgeesaraux <nethmalpaypal@gmail.com>
2025-09-15 20:29:28 +0530

    NavBar-Css and pages

--- frontend/src/component/Navbar/Navbar.jsx
+++ frontend/src/component/Navbar/Navbar.jsx
@@ -1,19 +1,55 @@
-import React from 'react'
-import './Navbar.css'
-import { assets } from '../../assets/assets'
+import React, { useState } from "react";
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
 
 const Navbar = () => {
+
+const[menu,setMenu]= useState("Home");
+
+
+
+
   return (
-    <div>
+    <div className="navbar">
       <img src={assets.logo} alt="" className="logo" />
-      <ul className="navbar-menu"></ul>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
+      <ul className="navbar-menu">
+        <li
+          className={menu === "Home" ? "acive" : ""}
+          onClick={() => setMenu("Home")}
+        >
+          Home
+        </li>
+        <li
+          className={menu === "Menu" ? "acive" : ""}
+          onClick={() => setMenu("Menu")}
+        >
+          Menu
+        </li>
+        <li
+          className={menu === "Mobile-App" ? "acive" : ""}
+          onClick={() => setMenu("Mobile-App")}
+        >
+          Mobile-App
+        </li>
+        <li
+          className={menu === "Contact-Us" ? "acive" : ""}
+          onClick={() => setMenu("Contact-Us")}
+        >
+          Contact Us
+        </li>
+      </ul>
 
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
+        </div>
+        <button>Sign Up</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;

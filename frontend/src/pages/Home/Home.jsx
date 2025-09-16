f11c390785951e39ceaa341cdc668a68546735e2
nethmalgeesaraux <nethmalpaypal@gmail.com>
2025-09-16 13:59:01 +0530

    Create ExploreMenu

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Home.css"
 import Header from '../../component/Hearder/Header'
+import ExploreMenu from '../../component/ExploreMenu/ExploreMenu'
 
 const Home = () => {
+
+  const[category,setCategory]=useState("All");
+
+
   return (
     <div>
         <Header/>
+        <ExploreMenu category={category}setCategory={setCategory}/>
     </div>
   )
 }

90c6110ada0ab0b7421b32d882694d161f97ae54
nethmalgeesaraux <nethmalpaypal@gmail.com>
2025-09-20 15:31:12 +0530

    Create Sing In

--- frontend/src/component/Navbar/Navbar.jsx
+++ frontend/src/component/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
+
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
 
-const Navbar = () => {
+const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState("Home");
 
   return (
@@ -55,7 +56,7 @@ const Navbar = () => {
           <img src={assets.basket_icon} alt="cart" />
           <div className="dot" />
         </div>
-        <button type="button">Sign Up</button>
+        <button type="button" onClick={() => setShowLogin?.(true)}>Sign Up</button>
       </div>
     </nav>
   );

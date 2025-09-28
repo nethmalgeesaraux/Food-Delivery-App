a488ed4492e6155dda6ceb343683a68a9b712dc3
nethmalgeesaraux <nethmalpaypal@gmail.com>
2025-09-28 07:57:51 +0530

    admin contoller menu

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Sidebar from './component/sidebar/Sidebar'
 import Navbar from './component/Navbar/Navbar'
+import { Routes ,Route } from 'react-router-dom'
+import Add from './pages/Add/Add'
+import List from './pages/List/List'
+import Orders from './pages/Orders/Orders' 
 
 const App = () => {
   return (
@@ -9,6 +13,11 @@ const App = () => {
       <hr/>
       <div className="app-content">
         <Sidebar/>
+        <Routes>
+          <Route path='/add' element = {<Add/>}/>
+          <Route path='/list' element = {<List/>}/>
+           <Route path='/orders' element = {<Orders/>}/>
+        </Routes>
       </div>
       
     </div>
